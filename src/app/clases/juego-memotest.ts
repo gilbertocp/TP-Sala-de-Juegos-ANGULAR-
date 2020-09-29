3faa83ddb5aa2ecf640afa42ec9d77347a919e63
Gilberto Calderón <calderongilberto3@gmail.com>
2020-09-29 15:47:48 -0300

    Lógica Memotest

--- src/app/clases/juego-memotest.ts
+++ src/app/clases/juego-memotest.ts
@@ -1,9 +1,42 @@
 import { Juego } from './juego';
 export class JuegoMemotest extends Juego {
+
+  cartas = [
+    {img: 'assets/imagenes/batido.png',      nombre: 'batido'},
+    {img: 'assets/imagenes/fritas.png',      nombre: 'fritas'},
+    {img: 'assets/imagenes/helado.png',      nombre: 'helado'},
+    {img: 'assets/imagenes/pancho.png',      nombre: 'pancho'},
+    {img: 'assets/imagenes/pizza.png',       nombre: 'pizza'},
+    {img: 'assets/imagenes/hamburguesa.png', nombre: 'hamburguesa'},
+    {img: 'assets/imagenes/batido.png',      nombre: 'batido'},
+    {img: 'assets/imagenes/fritas.png',      nombre: 'fritas'},
+    {img: 'assets/imagenes/helado.png',      nombre: 'helado'},
+    {img: 'assets/imagenes/pancho.png',      nombre: 'pancho'},
+    {img: 'assets/imagenes/pizza.png',       nombre: 'pizza'},
+    {img: 'assets/imagenes/hamburguesa.png', nombre: 'hamburguesa'},
+  ];
+
   constructor(nombre?: string, gano?: boolean, jugador?: string) {
     super('Memotest', gano, jugador);
   }
+  
   verificar(): boolean {
     return true;
+  }
+
+  ordenarCartas(): void {
+    this.cartas.sort(() => 0.5 * Math.random());
+  }
+
+  // algoritmo Fisher–Yates shuffle 
+  barajarCartas(a: any[]): any[] {
+    let j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
   }
 }
--- src/app/componentes/memotest/memotest.component.ts
+++ src/app/componentes/memotest/memotest.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JuegoMemotest } from '../../clases/juego-memotest';
 
 @Component({
   selector: 'app-memotest',
@@ -7,122 +8,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemotestComponent implements OnInit {
 
-  cardArray = [
-    {
-      name: 'fritas',
-      img: 'assets/imagenes/fritas.png'
-    },
-    {
-      name: 'hamburguesa',
-      img: 'assets/imagenes/hamburguesa.png'
-    },
-    {
-      name: 'helado',
-      img: 'assets/imagenes/helado.png'
-    },
-    {
-      name: 'pizza',
-      img: 'assets/imagenes/pizza.png'
-    },
-    {
-      name: 'batido',
-      img: 'assets/imagenes/batido.png'
-    },
-    {
-      name: 'pancho',
-      img: 'assets/imagenes/pancho.png'
-    },
-    {
-      name: 'papas',
-      img: 'assets/imagenes/papas.png'
-    },
-    {
-      name: 'hamburguesa',
-      img: 'assets/imagenes/hamburguesa.png'
-    },
-    {
-      name: 'helado',
-      img: 'assets/imagenes/helado.png'
-    },
-    {
-      name: 'pizza',
-      img: 'assets/imagenes/pizza.png'
-    },
-    {
-      name: 'batido',
-      img: 'assets/imagenes/batido.png'
-    },
-    {
-      name: 'pancho',
-      img: 'assets/imagenes/pancho.png'
+  juegoNuevo: JuegoMemotest;
+  cartasSeleccionadas = [];
+  cartasGanadas = [];
+
+  constructor() {
+    this.empezarJuego();
+  }
+
+  ngOnInit() {
+  }
+
+  empezarJuego(): void {
+    this.juegoNuevo = new JuegoMemotest();
+    this.juegoNuevo.cartas = this.juegoNuevo.barajarCartas(this.juegoNuevo.cartas);
+    this.cartasSeleccionadas = [];
+    this.cartasGanadas = [];
+  }
+
+  verificarEmparejamiento(): void {
+    const cartaUno: HTMLDivElement = this.cartasSeleccionadas[0];
+    const cartaDos: HTMLDivElement = this.cartasSeleccionadas[1];
+
+    const imgUno = cartaUno.children[1].children[0] as HTMLImageElement;
+    const imgDos = cartaDos.children[1].children[0] as HTMLImageElement;
+
+
+    if(cartaUno.id === cartaDos.id) {
+      this.cartasSeleccionadas = [];
+      return;
     }
-  ];
-
-  grid;
-  resultDisplay ;
-  cardsChosen = []
-  cardsChosenId = []
-  cardsWon = []
-
-
-  constructor() { 
-    this.cardArray.sort(() => 0.5 - Math.random())
-  }
-
-  ngOnInit(): void {
-    this.grid = document.querySelector('.grid')
-    this.resultDisplay = document.querySelector('#result')
-    this.createBoard();
-  }
-
-
-  createBoard() {
-    for (let i = 0; i < this.cardArray.length; i++) {
-      var card = document.createElement('img')
-      card.setAttribute('src', '/assets/imagenes/blank.png')
-      card.setAttribute('data-id', i.toString())
-      card.addEventListener('click', this.flipCard)
-      this.grid.appendChild(card)
-    }
-  }
-
-   //check for matches
-  checkForMatch() {
-    var cards = document.querySelectorAll('img')
-    const optionOneId = this.cardsChosenId[0]
-    const optionTwoId = this.cardsChosenId[1]
     
-    if(optionOneId == optionTwoId) {
-      cards[optionOneId].setAttribute('src', '/assets/imagenes/blank.png')
-      cards[optionTwoId].setAttribute('src', '/assets/imagenes/blank.png')
+    if(imgUno.name === imgDos.name) {
+      cartaUno.className += ' emparejado';
+      cartaDos.className += ' emparejado';
+      this.cartasGanadas.push(imgUno);
     }
-    else if (this.cardsChosen[0] === this.cardsChosen[1]) {
-      alert('Encontraste una pareja')
-      cards[optionOneId].setAttribute('src', '/assets/imagenes/white.png')
-      cards[optionTwoId].setAttribute('src', '/assets/imagenes/white.png')
-      cards[optionOneId].removeEventListener('click', this.flipCard)
-      cards[optionTwoId].removeEventListener('click', this.flipCard)
-      this.cardsWon.push(this.cardsChosen)
-    } else {
-      cards[optionOneId].setAttribute('src', '/assets/imagenes/blank.png')
-      cards[optionTwoId].setAttribute('src', '/assets/imagenes/blank.png')
-      alert('Intentelo De nuevo')
+    else {
+      cartaUno.style.transform = '';
+      cartaDos.style.transform = '';
     }
-    this.cardsChosen = []
-    this.cardsChosenId = []
-    this.resultDisplay.textContent = this.cardsWon.length.toString();
-    if  (this.cardsWon.length === this.cardArray.length/2) {
-      this.resultDisplay.textContent = 'Ganaste'
+
+    this.cartasSeleccionadas = [];
+
+    if(this.cartasGanadas.length === this.juegoNuevo.cartas.length / 2) {
+      alert('Has ganado');
     }
   }
 
-  flipCard(e) {
-    var cardId = e.target.getAttribute('data-id')
-    this.cardsChosen.push(this.cardArray[cardId].name)
-    this.cardsChosenId.push(cardId)
-    e.target.setAttribute('src', this.cardArray[cardId].img)
-    if (this.cardsChosen.length ===2) {
-      setTimeout(this.checkForMatch, 500)
-    }    
+  voltearCarta(el: HTMLDivElement): void {
+    if(el.classList.contains('emparejado'))
+      return;
+
+    if(this.cartasSeleccionadas.length < 2) {
+      el.style.transform = !el.style.transform? 'rotateY(180deg)': '';
+      this.cartasSeleccionadas.push(el);
+    }
+    
+    if(this.cartasSeleccionadas.length === 2) {
+      setTimeout(() => this.verificarEmparejamiento(), 500);
+    }
   }
+
 }
