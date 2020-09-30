ad24cbee1dd5c6af97f3775ee22a601f1941539f
Gilberto Calderón <calderongilberto3@gmail.com>
2020-09-30 00:50:06 -0300

    Mejoro lógica Tateti

--- src/app/componentes/tateti/tateti.component.ts
+++ src/app/componentes/tateti/tateti.component.ts
@@ -14,37 +14,72 @@ export class TatetiComponent implements OnInit {
   juegoIniciado: boolean;
   turno: boolean; // si es true el turno de juegador, caso contrario false
 
+  celdasDibujadas: HTMLDivElement[] = [];
+  celdasDibujadasUsuario: HTMLDivElement[] = [];
+  celdasDibujadasComputadora: HTMLDivElement[] = [];
+
   constructor() {
-    this.nuevoJuego = new JuegoTateti();
-    this.juegoGanado = false;
-    this.juegoIniciado = false;
   }
 
   ngOnInit(): void {
   }
 
-  dibujar(e: Event): void {
-    if (this.turno) {
-      (e.target as HTMLDivElement).className += ` ${this.nuevoJuego.seleccionUsuario}`;
-      (e.target as HTMLDivElement).innerHTML = `<span class="display-2">${this.nuevoJuego.seleccionUsuario === 'Circulo' ? 'O' : 'X'}</span>`;
+  cambiarTurnos(): void {
+    this.turno = !this.turno;
 
-      if (this.verificarJuego()) {
-        return;
-      }
+    if(this.turno === false) 
+      setTimeout(() => this.dibujarComputadora(), 250);
+  }
 
-      this.turno = !this.turno;
-      this.figuraOponente();
-    }else {
-      if (e === null) {
-        this.figuraOponente();
-      }
+  dibujarUsuario(el: HTMLDivElement): void {
+
+    if(this.turno !== true) {
+      alert('Espera que no es tu turno');
+      return;
     }
+
+    const celdaYaEstaDibujada = this.celdasDibujadas.some(celda => celda.id === el.id);
+    
+    if(celdaYaEstaDibujada) {
+      alert('Esta celda ya esta dibujada escoje otra');
+      return;
+    }
+
+    el.className += ' ' + this.nuevoJuego.seleccionUsuario;
+    el.innerHTML += this.nuevoJuego.seleccionUsuario === 'Equis' ? 'X' : 'O';
+    
+    this.celdasDibujadas.push(el);
+    this.celdasDibujadasUsuario.push(el);
+
+    if(!this.verificarJuego())
+      this.cambiarTurnos();
+  }
+
+  dibujarComputadora(): void {
+    const celdas = Array.from(document.getElementsByClassName('celda')) as HTMLDivElement[];
+    
+    const celdasVacias = celdas.filter(cel => {
+      return !this.celdasDibujadas.some(cel2 => cel2.id === cel.id);
+    });
+
+    const celdaADibujar = celdasVacias[Math.floor(Math.random() * celdasVacias.length)];
+
+    celdaADibujar.className += ' ' + this.nuevoJuego.seleccionComputadora;
+    celdaADibujar.innerHTML += this.nuevoJuego.seleccionComputadora === 'Equis'? 'X' : 'O';
+
+    this.celdasDibujadas.push(celdaADibujar);
+    this.celdasDibujadasComputadora.push(celdaADibujar);
+
+    if(!this.verificarJuego())
+      this.cambiarTurnos();
   }
 
   figuraSeleccionada(e: Event): void {
     const clases = (e.currentTarget as HTMLButtonElement).classList;
-    this.turno = ([true, false])[Math.floor(Math.random() * 2)];
     this.nuevoJuego = new JuegoTateti();
+    
+    // Elegir aletoriamente el turno (True => usuario, False => CPU)
+    this.turno = ([true, false])[Math.floor(Math.random() * 2)];
 
     clases.forEach((clase: string) => {
       if (clase === 'btnCirculo') {
@@ -59,66 +94,49 @@ export class TatetiComponent implements OnInit {
     });
 
     this.juegoIniciado = true;
-    if (!this.turno) {
-      setTimeout(() => this.dibujar(null), 350);
-    }
+    this.turno = true;
+    // if (!this.turno) {
+    //   setTimeout(() => this.dibujarComputadora(), 150);
+    // }
   }
 
-  figuraOponente(): void {
-    if (!this.turno){
-      const celdas = document.querySelectorAll('.celda') as NodeListOf<HTMLDivElement>;
-      timer(1000).subscribe(() => {
-        for (const el of Array.from(celdas)) {
-          if (!el.innerHTML){
-            el.className += ` ${this.nuevoJuego.seleccionComputadora}`;
-            el.innerHTML =  `<span class="display-2">${this.nuevoJuego.seleccionComputadora === 'Circulo' ? 'O' : 'X'}</span>`;
-            break;
-          }
-        }
-
-        if (this.verificarJuego()) {
-          return;
-        }
-
-        this.turno = !this.turno;
-      });
-    }
-  }
-
-  verificarGanador(figura: string): boolean {
-    const celdas = document.querySelectorAll('.celda') as NodeListOf<HTMLDivElement>;
-
+  verificarGanador(celdasDibujadas: HTMLDivElement[]): boolean {
     return this.nuevoJuego.COMBINACIONES_GANAR.some(combinacion => {
-      return combinacion.every(idx => (Array.from(celdas)[idx]).classList.contains(figura));
+      let contador = 0;
+
+      celdasDibujadas.forEach(celda => {
+        if( combinacion.includes(parseInt(celda.id)) ) {
+          contador++;
+        }
+      });
+
+      return contador === 3;
     });
   }
 
-  empate(): boolean {
-    const celdas = document.querySelectorAll('.celda') as NodeListOf<HTMLDivElement>;
-    return Array.from(celdas).every(celda => celda.classList.contains('Circulo') || celda.classList.contains('Equis'));
+  celdasLlenas(): boolean {
+    return this.celdasDibujadas.length === document.getElementsByClassName('celda').length;
   }
 
   verificarJuego(): boolean {
-    let ret = false;
-
-    if (this.verificarGanador(this.turno ? this.nuevoJuego.seleccionUsuario : this.nuevoJuego.seleccionComputadora)) {
-      this.nuevoJuego.gano = this.turno ? true : false;
-      ret = true;
+    let finDelJuego = false;
+    
+    if(this.verificarGanador(this.celdasDibujadasUsuario)){
+      alert('El usuario gana')
+      finDelJuego = true
     }
-    else if (this.empate()) {
-      this.nuevoJuego.gano = false;
-      ret = true;
+    else if(this.verificarGanador(this.celdasDibujadasComputadora)){
+      alert('Computadora gana')
+      finDelJuego = true;
+    }
+    else if(this.celdasLlenas()) {
+      alert('Hay empate');
+      finDelJuego = true;
     }
 
-    if (ret) {
-      this.finDelJuego();
-    }
-
-    return ret;
+    return finDelJuego;
   }
 
   finDelJuego(): void {
-    (document.querySelector('#botonVentanaModal') as HTMLButtonElement).click();
-    this.juegoIniciado = !this.juegoIniciado;
   }
 }
